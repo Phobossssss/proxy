2c34b09daf17732e34748dee393c3c04b8f37b48
Phobossssss <95530165+Phobossssss@users.noreply.github.com>
2021-12-14 02:53:06 +0800

    Update bilibili_ad.js

--- QuantumultX/rewrite/js/bilibili_ad.js
+++ QuantumultX/rewrite/js/bilibili_ad.js
@@ -35,13 +35,6 @@ if (magicJS.read(blackKey)) {
                 item["banner_item"] = bannerItems;
                 items.push(item);
               }
-            } else if (
-              !item.hasOwnProperty("ad_info") &&
-              !blacklist.includes(item["args"]["up_name"]) &&
-              item.card_goto.indexOf("将减少相似广告推荐") === -1 &&
-              (item["card_type"] === "small_cover_v2" || item["card_type"] === "large_cover_v1")
-            ) {
-              items.push(item);
             }
           }
           obj["data"]["items"] = items;
