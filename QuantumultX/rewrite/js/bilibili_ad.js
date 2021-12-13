3e1094d199b12e401ab3c189ea3143129cca7798
Phobossssss <95530165+Phobossssss@users.noreply.github.com>
2021-12-14 02:42:45 +0800

    Update bilibili_ad.js

--- QuantumultX/rewrite/js/bilibili_ad.js
+++ QuantumultX/rewrite/js/bilibili_ad.js
@@ -22,6 +22,34 @@ if (magicJS.read(blackKey)) {
         try {
           let obj = JSON.parse(magicJS.response.body);
           let items = [];
+          for (let item of obj["data"]["items"]) {
+            if (item.hasOwnProperty("banner_item")) {
+              let bannerItems = [];
+              for (let banner of item["banner_item"]) {
+                if (banner["type"] === "ad") {
+                  continue;
+                } else if (banner["static_banner"] && banner["static_banner"]["is_ad_loc"] != true) {
+                  bannerItems.push(banner);
+                }
+              }
+              // 去除广告后，如果banner大于等于1个才添加到响应体
+              if (bannerItems.length >= 1) {
+                item["banner_item"] = bannerItems;
+                items.push(item);
+              }
+            } else if (
+              !item.hasOwnProperty("ad_info") &&
+              !blacklist.includes(item["args"]["up_name"]) &&
+              item.card_goto.indexOf("ad") === -1 &&
+              (item["card_type"] === "small_cover_v2" || item["card_type"] === "large_cover_v1")
+            ) {
+              items.push(item);
+            }
+          }
+          obj["data"]["items"] = items;
+          body = JSON.stringify(obj);
+        } catch (err) {
+          magicJS.logError(`推荐去广告出现异常：${err}`);
            }
         break;
     
