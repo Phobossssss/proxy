ed020804fe987210b117b9cc86016e9432b37f89
Phobossssss <95530165+Phobossssss@users.noreply.github.com>
2022-06-09 17:04:53 +0800

    Update bilibili_ad.js

--- QuantumultX/rewrite/js/bilibili_ad.js
+++ QuantumultX/rewrite/js/bilibili_ad.js
@@ -26,7 +26,7 @@ if (magicJS.read(blackKey)) {
             if (item.hasOwnProperty("banner_item")) {
               let bannerItems = [];
               for (let banner of item["banner_item"]) {
-                if (banner["type"] === "ad") {
+                if (banner["type"] === "ad"|| !item["card_type"] === "large_cover_v1") {
                   continue;
                 } else if (banner["static_banner"] && banner["static_banner"]["is_ad_loc"] != true) {
                   bannerItems.push(banner);
